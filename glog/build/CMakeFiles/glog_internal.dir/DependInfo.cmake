6b909de942aefcedd59e0f75f1fa82592fa72e64
Artem Sevastopolsky <seva100@users.noreply.github.com>
2023-07-11 13:50:40 +0000

    glog build

---
+++ glog/build/CMakeFiles/glog_internal.dir/DependInfo.cmake
@@ -0,0 +1,25 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/workspaces/3DSMC-exercise-5-core/glog/src/demangle.cc" "CMakeFiles/glog_internal.dir/src/demangle.cc.o" "gcc" "CMakeFiles/glog_internal.dir/src/demangle.cc.o.d"
+  "/workspaces/3DSMC-exercise-5-core/glog/src/logging.cc" "CMakeFiles/glog_internal.dir/src/logging.cc.o" "gcc" "CMakeFiles/glog_internal.dir/src/logging.cc.o.d"
+  "/workspaces/3DSMC-exercise-5-core/glog/src/raw_logging.cc" "CMakeFiles/glog_internal.dir/src/raw_logging.cc.o" "gcc" "CMakeFiles/glog_internal.dir/src/raw_logging.cc.o.d"
+  "/workspaces/3DSMC-exercise-5-core/glog/src/signalhandler.cc" "CMakeFiles/glog_internal.dir/src/signalhandler.cc.o" "gcc" "CMakeFiles/glog_internal.dir/src/signalhandler.cc.o.d"
+  "/workspaces/3DSMC-exercise-5-core/glog/src/symbolize.cc" "CMakeFiles/glog_internal.dir/src/symbolize.cc.o" "gcc" "CMakeFiles/glog_internal.dir/src/symbolize.cc.o.d"
+  "/workspaces/3DSMC-exercise-5-core/glog/src/utilities.cc" "CMakeFiles/glog_internal.dir/src/utilities.cc.o" "gcc" "CMakeFiles/glog_internal.dir/src/utilities.cc.o.d"
+  "/workspaces/3DSMC-exercise-5-core/glog/src/vlog_is_on.cc" "CMakeFiles/glog_internal.dir/src/vlog_is_on.cc.o" "gcc" "CMakeFiles/glog_internal.dir/src/vlog_is_on.cc.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
