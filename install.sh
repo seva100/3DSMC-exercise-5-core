816cfe1742978eecbe71b905c226c72c17826e66
Artem Sevastopolsky <seva100@users.noreply.github.com>
2023-11-24 14:57:32 +0100

    ceres through apt-get + latest cmake

--- install.sh
+++ install.sh
@@ -1,6 +1,14 @@
 sudo apt update
-# sudo apt-get install cmake libgoogle-glog-dev libgflags-dev libatlas-base-dev libeigen3-dev libsuitesparse-dev build-essential g++ gdb python3-dev autotools-dev libicu-dev libbz2-dev libboost-all-dev libflann-dev libfreeimage3 libfreeimage-dev
-sudo apt-get install cmake libgflags-dev libatlas-base-dev libeigen3-dev libsuitesparse-dev build-essential g++ gdb python3-dev autotools-dev libicu-dev libbz2-dev libboost-all-dev libflann-dev libfreeimage3 libfreeimage-dev lz4
+
+# installing the latest cmake
+wget https://github.com/Kitware/CMake/releases/download/v3.28.0-rc5/cmake-3.28.0-rc5-linux-x86_64.tar.gz
+tar xf cmake-3.28.0-rc5-linux-x86_64.tar.gz
+cd cmake-3.28.0-rc5-linux-x86_64
+export PATH=`pwd`:$PATH
+
+# apt-get: dependencies for Ceres, Ceres itself, FLANN, Boost, Eigen, etc.
+sudo apt-get install libgflags-dev libatlas-base-dev libeigen3-dev libsuitesparse-dev build-essential g++ gdb python3-dev \
+    autotools-dev libicu-dev libbz2-dev libboost-all-dev libflann-dev libfreeimage3 libfreeimage-dev lz4 libceres-dev
 
 # VSCode: install extensions CMake, CMake Tools, C/C++ Extension Pack
 
@@ -12,3 +20,5 @@ cmake -S . -B build -G "Unix Makefiles"
 cmake --build build
 sudo cmake --build build --target install
 # sudo ln -s /usr/lib/x86_64-linux-gnu/libglog.so.0 /usr/lib/x86_64-linux-gnu/libglog.so
+
+cd ..
